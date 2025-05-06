fc62242411736e3e9f94e6788aaafe0172c85040
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-05-06 10:47:26 +0700

    Menambahkan class Lingkaran

--- contoh.cpp
+++ contoh.cpp
@@ -23,3 +23,21 @@ public:
     Lingkaran o1;
      
 };
+
+class Lingkaran 
+{
+    public: 
+        int jarijari;
+
+        void input()
+        {
+            cout << "Masukkan jari-jari : ";
+            cin >> jarijari;
+        }
+
+        int luasLingkaran(int r)
+        {
+            return 3,14 * r *r;
+
+        }
+};
